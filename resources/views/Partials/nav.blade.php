27dcc9acf9ddefa77c931dba900a6534494a73d5
ZeeNthakheni <30797347+ZeeNthakheni@users.noreply.github.com>
2018-08-16 21:32:34 +0200

    ex

--- resources/views/Partials/nav.blade.php
+++ resources/views/Partials/nav.blade.php
@@ -1,5 +1,6 @@
 <nav class="navbar navbar-light navbar-expand-md">
-        <div class="container-fluid"><a href="/" class="navbar-brand">Brand</a><button data-toggle="collapse" data-target="#navcol-1" class="navbar-toggler"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+        <div class="container-fluid"><a href="/" class="navbar-brand">Brand</a><button data-toggle="collapse" data-target="#navcol-1" class="navbar-toggler">
+            <span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
             <div class="collapse navbar-collapse"
                 id="navcol-1">
                 <ul class="nav navbar-nav ml-auto">
