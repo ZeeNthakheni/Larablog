9bc723dc57959e19ac49f3a02b6a7d57f3914bf2
ZeeNthakheni <30797347+ZeeNthakheni@users.noreply.github.com>
2018-08-21 14:25:59 +0200

    Auguts 8 3rd commit

--- resources/views/Pages/index.blade.php
+++ resources/views/Pages/index.blade.php
@@ -1,13 +1,13 @@
 @extends('layout.layout')
 
 @section('content')
-    <div class="jumbotron">
+    <div class="jumbotron text-center">
         <h1 class="display-4">Welcome to my Laravel blog site!</h1>
         <p class="lead">This is my Laravel blog site stuff</p>
         <hr class="my-4">
         <p class="lead">
-          <a class="btn btn-primary btn-lg" href="#" role="button">Login</a>
-          <a class="btn btn-primary btn-lg" href="#" role="button">Register</a>
+          <a class="btn btn-primary btn-lg" href="/login" role="button">Login</a>
+          <a class="btn btn-success btn-lg" href="/register" role="button">Register</a>
         </p>
       </div>
 @endsection
