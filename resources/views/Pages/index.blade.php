dff0453f789b626ecd7f932e96ebed21bdb726ba
ZeeNthakheni <30797347+ZeeNthakheni@users.noreply.github.com>
2018-08-21 13:58:51 +0200

    August 21 1st commit

--- resources/views/Pages/index.blade.php
+++ resources/views/Pages/index.blade.php
@@ -1,6 +1,13 @@
 @extends('layout.layout')
 
 @section('content')
-    <h1>Welcome to my Laravel blog site</h1>
-    <p>This is my Laravel blog site stuff</p> 
+    <div class="jumbotron">
+        <h1 class="display-4">Welcome to my Laravel blog site!</h1>
+        <p class="lead">This is my Laravel blog site stuff</p>
+        <hr class="my-4">
+        <p class="lead">
+          <a class="btn btn-primary btn-lg" href="#" role="Login">Learn more</a>
+          <a class="btn btn-primary btn-lg" href="#" role="Register">Learn more</a>
+        </p>
+      </div>
 @endsection
--- resources/views/Partials/nav.blade.php
+++ resources/views/Partials/nav.blade.php
@@ -1,8 +1,10 @@
 <nav class="navbar navbar-light navbar-expand-md">
-        <div class="container-fluid"><a href="/" class="navbar-brand">Brand</a><button data-toggle="collapse" data-target="#navcol-1" class="navbar-toggler">
-            <span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
-            <div class="collapse navbar-collapse"
-                id="navcol-1">
+        <div class="container-fluid"><a href="/" class="navbar-brand">{{config('app.name','LSAPP')}}</a>
+            <button data-toggle="collapse" data-target="#navcol-1" class="navbar-toggler">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navcol-1">
                 <ul class="nav navbar-nav ml-auto">
                     <li role="presentation" class="nav-item"><a href="/" class="nav-link active">Home</a></li>
                     <li role="presentation" class="nav-item"><a href="/about" class="nav-link active">About</a></li>
