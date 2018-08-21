6ca0cb9bfca90476c03ea211715f772ad712e17a
ZeeNthakheni <30797347+ZeeNthakheni@users.noreply.github.com>
2018-08-21 14:01:58 +0200

    August 8 2nd commit

--- resources/views/Pages/index.blade.php
+++ resources/views/Pages/index.blade.php
@@ -6,8 +6,8 @@
         <p class="lead">This is my Laravel blog site stuff</p>
         <hr class="my-4">
         <p class="lead">
-          <a class="btn btn-primary btn-lg" href="#" role="Login">Learn more</a>
-          <a class="btn btn-primary btn-lg" href="#" role="Register">Learn more</a>
+          <a class="btn btn-primary btn-lg" href="#" role="button">Login</a>
+          <a class="btn btn-primary btn-lg" href="#" role="button">Register</a>
         </p>
       </div>
 @endsection
